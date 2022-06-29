a49160d006df86eda42a993cca30471252beb473
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-06-29 22:44:09 +0300

    some changes were made

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,9 +5,9 @@ import './Card.css'
 const Card = () => {
   return (
     <div className="cards-area-container">
-      <div className="bars"></div>
-      <h1 className="language-title">Languages</h1>
-      <div className="cards-container">
+      <div className="bars" ></div>
+      <h1 className="language-title" >Languages</h1>
+      <div className="cards-container" >
         {
             languages.map((item, index) => {
                 return (
